2bf662ad868d676021ee28b7072120082b6744a8
demacedius <demacedius@gmail.com>
2020-09-06 12:33:29 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use ggez::conf::WindowMode;
+use ggez::{event, ContextBuilder};
+use ggez_tuto::GameState;
+fn main() {
+    let (context, event_loop) =
+        &mut match ContextBuilder::new("first_game_ggez", "de Macedo Anthony")
+            .window_mode(WindowMode::default().dimensions(1920.0, 1080.0))
+            .build()
+        {
+            Ok((context, event_loop)) => (context, event_loop),
+            Err(error) => panic!(error),
+        };
+
+    let game_state = &mut GameState::new();
+
+    match event::run(context, event_loop, game_state) {
+        Ok(_) => println!("Merci d'avoir jouer"),
+        Err(error) => println!("L'erreur est: {}", error),
+    };
+}
